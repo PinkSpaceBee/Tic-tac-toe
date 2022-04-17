9cc29d695ad2f1c0f0fee99d97f56c5b6e9b3c4f
name <minaary@mailfence.com>
2022-04-17 17:46:04 +0200

    make factory function for creating players

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// const Player = (mark) = {
-    
-// }
+const player = () => {
+    return {
+        mark: this.mark,
+        chooseMark() {
+            const markBtns = Array.from(document.querySelectorAll('.js-choose-mark-btn'));
+
+            markBtns.forEach(btn => btn.addEventListener('click', (e) => {
+                this.mark = e.target.id;
+            }));
+        }
+    }
+}
 
 const gameBoard = (function() {
     function render() {
@@ -17,3 +26,33 @@ return {
 })();
 
 gameBoard.render();
+
+const humanPlayer = player();
+humanPlayer.chooseMark();
+console.log(`human ${humanPlayer.mark}`);
+
+
+//const computerPlayer  = player();
+//computerPlayer.mark = humanPlayer.mark = 'x' ? 'o' : 'x';
+
+//console.log(`human: ${humanPlayer.mark}; computer: ${computerPlayer.mark}`)
+
+/*
+const greeter = () => {
+    return {
+        mark: this.mark, 
+        greet() {
+            const btns = Array.from(document.querySelectorAll('.x'));
+            btns.forEach(btn => btn.addEventListener('click', (e) => {
+                this.mark = e.target.id;
+                //name = 'q';
+                //console.log(e.target)
+            }))
+            //return `Hi, my name is ${name}`;
+        }
+    }
+}
+
+const human = greeter();
+human.greet();
+*/
