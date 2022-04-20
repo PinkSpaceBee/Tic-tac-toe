f2e8a71c69bc63c27bd61dcc215632ae5d9f644e
name <minaary@mailfence.com>
2022-04-20 21:04:29 +0200

    add function to disable mark buttons when user selects a mark

--- script.js
+++ script.js
@@ -8,13 +8,21 @@ const player = () => {
         computerPlayer.mark = humanPlayer.mark === 'x' ? 'o' : 'x';
     }
 
+    // disable buttons when player selects a mark
+    function disableButtons() {
+        markBtns.forEach(btn => btn.addEventListener('click', () => {markBtns.forEach(btn => btn.disabled = true)}, false));
+    }
+
     return {
         mark: this.mark,
         chooseMark() {
             markBtns.forEach(btn => btn.addEventListener('click', (e) => {
                 this.mark = e.target.id;
             }, false));
+            
             markBtns.forEach(btn => btn.addEventListener('click', assignMarkComputer, false));
+
+            disableButtons();
         }
     }
 }
