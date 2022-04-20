5d7ec957823bc065d500f1c306d10c23ad4631e5
name <minaary@mailfence.com>
2022-04-20 18:15:33 +0200

    add function to assign mark to computer

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+'use strict'
+
+// a factory function for creating players
 const player = () => {
+    const markBtns = Array.from(document.querySelectorAll('.js-choose-mark-btn'));
+
+    function assignMarkComputer() {
+        computerPlayer.mark = humanPlayer.mark === 'x' ? 'o' : 'x';
+    }
+
     return {
         mark: this.mark,
         chooseMark() {
-            const markBtns = Array.from(document.querySelectorAll('.js-choose-mark-btn'));
-
             markBtns.forEach(btn => btn.addEventListener('click', (e) => {
                 this.mark = e.target.id;
-            }));
+            }, false));
+            markBtns.forEach(btn => btn.addEventListener('click', assignMarkComputer, false));
         }
     }
 }
 
+// gameboard module
 const gameBoard = (function() {
     function render() {
         const gameBoardArray = Array.from(document.querySelectorAll('td'));
@@ -28,7 +37,6 @@ return {
 gameBoard.render();
 
 const humanPlayer = player();
-humanPlayer.chooseMark();
-
 const computerPlayer = player();
-computerPlayer.mark = humanPlayer.mark === 'x' ? 'o' : 'x';
+
+humanPlayer.chooseMark();
