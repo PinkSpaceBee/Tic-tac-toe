69875a4c2ef0d634f496079319ba9b04e49072b4
name <minaary@mailfence.com>
2022-05-16 18:04:50 +0200

    add getRandomCell(), fix order in which funcs are called

--- script.js
+++ script.js
@@ -19,30 +19,64 @@ return {
 const gameController = (function() {
     const gameboard = gameBoard.gameBoardArray;
 
-    function checkTurn() {
-        // if human is x { makeMoveHuman ?? what?? it listens to btn click anyway}
-        // if human is o {}
-    };
+    function checkTurnComputer() {
+        const humanMarks = gameboard.map(cell => cell.textContent)
+        .filter(cell => cell == humanPlayer.mark);
+
+        const computerMarks = gameboard.map(cell => cell.textContent)
+        .filter(cell => cell == computerPlayer.mark);
+
+        if (computerPlayer.mark === 'x') {
+            console.log(`c ${computerPlayer.mark} hMl ${humanMarks.length}`);
+            makeMoveComputer();
+        } else if (computerPlayer.mark === 'o') {
+            console.log(`c ${computerPlayer.mark} hMl ${humanMarks.length}`);
+            makeMoveComputer();
+        }
+        console.log(`checkTurnComputer was called`);
+    }
+
     function makeMoveComputer() {
         // count empty cells
-        // place a mark in a random empty cell 
-        console.log('makeMoveComputer was called')
-    };
-    function makeMoveHuman() {
+        function getEmptyCells() {
+            let emptyCells = gameboard.map(cell => cell.textContent)
+            .reduce((arr, elem, index) => {if (elem === '') arr.push(index); return arr;}, []);
+
+            console.log(`getEmptyCells was called ${emptyCells}`);
+        }
+
+        function getRandomCell(max) {
+            max = Math.floor(max);
+            console.log(`getRandomCell was called`);
+            return Math.floor(Math.random() * max);
+        }
+
+        getEmptyCells();
+        getRandomCell();
+
+        console.log('makeMoveComputer was called');
+    }
+
+    function makeMoveHuman(callback) {
+        const humanMarks = gameboard.map(cell => cell.textContent)
+        .filter(cell => cell === humanPlayer.mark);
+
         gameboard.forEach(cell => cell.addEventListener('click', (e) => {
             // place a mark in the selected cell if it's empty
             if (e.target.textContent === '') {
                 e.target.textContent = humanPlayer.mark;
+                console.log(`makeMoveHuman was called`);
             }
-        }))
-        console.log(`makeMoveHuman was called`);
+            callback();
+        }));
     };
 
     function determineWinner() {}
 
     return {
         makeMoveHuman,
-        makeMoveComputer
+        makeMoveComputer,
+        checkTurnComputer,
     }
 })();
 
@@ -50,44 +84,39 @@ const gameController = (function() {
 const player = () => {
     const markBtns = Array.from(document.querySelectorAll('.js-choose-mark-btn'));
 
-    function assignMarkComputer() {
+    function assignMarkComputer(callback) {
         computerPlayer.mark = humanPlayer.mark === 'x' ? 'o' : 'x';
         console.log(`assignMarkComp was called comp ${computerPlayer.mark}`);
+        callback();
     }
 
     // disable buttons when player selects a mark
     function disableButtons() {
         markBtns.forEach(btn => btn.addEventListener('click', () => {markBtns.forEach(btn => btn.disabled = true)}, false));
     }
+
     disableButtons();
 
     return {
         mark: this.mark,
-        // do I need to make markBtns public? check later 
-        //markBtns,
-        chooseMark(callback1, callback2) {
+        chooseMark() {
             markBtns.forEach(btn => btn.addEventListener('click', (e) => {
                 this.mark = e.target.id;
                 console.log(`chooseMark was called human ${humanPlayer.mark}`);
                 // as soon as human selects a mark, computer gets a mark assigned to it
-                assignMarkComputer();
-                callback1();
-                callback2();
+                assignMarkComputer(gameController.checkTurnComputer);
             }, false));
         }
     }
 }
 
-//gameBoard.render();
-
 const humanPlayer = player();
 const computerPlayer = player();
 
-humanPlayer.chooseMark(gameController.makeMoveHuman, gameController.makeMoveComputer);
+humanPlayer.chooseMark();
 
-
-//gameController.makeMoveHuman();
-//gameController.makeMoveComputer();
+gameController.makeMoveHuman(gameController.makeMoveComputer);
+//gameController.checkTurnComputer();
 
 //oh great I broke something. 
 //Found a bag. Apparently if randomCell equals 0, computer can make the first move even if its mark is o. Yep, I managed to reproduce it. Wellll, how do I fix it? I suppose the computer thinks that the cell is empty and makes a move. Player, in his turn, sees that the cell is taken and doesn't mark it. I don't like that the functions execute in the wrong order. If not for that, I wouldn't encounter these issues. 
